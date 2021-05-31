44bc9c30901250fcf90985a7120d0932906516c8
Mamikon Arakelyan <m.araqelyan@gmail.com>
2021-06-01 00:47:09 +0400

    Added test for testing that TaskId can be created only by named constructor

--- Tests/TaskManagement/Domain/TaskIdTest.php
+++ Tests/TaskManagement/Domain/TaskIdTest.php
@@ -1,11 +1,19 @@
 <?php
 
 
+use TaskManagement\Domain\TaskId;
+
 class TaskIdTest extends \PHPUnit\Framework\TestCase
 {
     public function test_task_id_can_be_generated()
     {
         $taskId = \TaskManagement\Domain\TaskId::generate();
         $this->assertTrue(\Ramsey\Uuid\Uuid::isValid($taskId->toString()));
+    }
+
+    public function test_task_id_must_be_created_thue_named_constructor()
+    {
+        $this->expectException(\Error::class);
+        $taskId = new TaskId("test");
     }
 }
