8dff6aca8401b981ba5fc89253ffeab2524271ab
Mamikon Arakelyan <m.araqelyan@gmail.com>
2021-06-01 01:50:25 +0400

    test that title value object can be created from string

--- Tests/TaskManagement/Domain/Task/TitleTest.php
+++ Tests/TaskManagement/Domain/Task/TitleTest.php
@@ -3,5 +3,11 @@
 
 class TitleTest extends \PHPUnit\Framework\TestCase
 {
+    public function test_that_title_can_be_created_from_string()
+    {
+        $titleString = "Test";
+        $title       = \TaskManagement\Domain\Task\Title::fromString($titleString);
+        $this->assertSame($titleString, $title->toString());
+    }
 
 }
