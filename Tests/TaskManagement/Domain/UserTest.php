ccf87067f73496d63d160f9c2284e342383b4a50
Mamikon Arakelyan <m.araqelyan@gmail.com>
2021-06-01 01:13:16 +0400

    Test added for user creation from string

--- Tests/TaskManagement/Domain/UserTest.php
+++ Tests/TaskManagement/Domain/UserTest.php
@@ -3,8 +3,10 @@
 
 class UserTest extends \PHPUnit\Framework\TestCase
 {
-    public function test_user_can_be_created()
+    public function test_user_can_be_created_from_string()
     {
-
+        $uuid = \Ramsey\Uuid\Uuid::uuid4()->toString();
+        $user = \TaskManagement\Domain\User::fromString($uuid);
+        $this->assertSame($user->toString(), $uuid);
     }
 }
