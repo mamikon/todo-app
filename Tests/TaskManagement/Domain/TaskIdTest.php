c88bdf5e74ffc5a27e9be0cc099d794a81db6212
Mamikon Arakelyan <m.araqelyan@gmail.com>
2021-06-01 00:25:50 +0400

    Namespace fixed for task id value object

--- Tests/TaskManagement/Domain/TaskIdTest.php
+++ Tests/TaskManagement/Domain/TaskIdTest.php
@@ -5,7 +5,7 @@ class TaskIdTest extends \PHPUnit\Framework\TestCase
 {
     public function test_task_id_can_be_generated()
     {
-        $taskId = \TaskManagement\TaskId::generate();
+        $taskId = \TaskManagement\Domain\TaskId::generate();
         $this->assertTrue(\Ramsey\Uuid\Uuid::isValid($taskId->toString()));
     }
 }
