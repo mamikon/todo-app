7de0f7bcbe1baba0089412e38b2755b429ba1a52
Mamikon Arakelyan <m.araqelyan@gmail.com>
2021-06-02 02:27:11 +0400

    Task update handler created

--- src/TaskManagement/Application/Command/Task/TaskUpdateHandler.php
+++ src/TaskManagement/Application/Command/Task/TaskUpdateHandler.php
@@ -4,7 +4,32 @@
 namespace TaskManagement\Application\Command\Task;
 
 
+use TaskManagement\Domain\Task\Date;
+use TaskManagement\Domain\Task\Description;
+use TaskManagement\Domain\Task\Status;
+use TaskManagement\Domain\Task\Task;
+use TaskManagement\Domain\Task\TaskId;
+use TaskManagement\Domain\Task\TaskService;
+use TaskManagement\Domain\Task\Title;
+use TaskManagement\Domain\Task\User;
+
 class TaskUpdateHandler
 {
+    public function __construct(private TaskService $taskService)
+    {
 
+    }
+
+    public function __invoke(TaskUpdateCommand $command)
+    {
+        $task = Task::create(
+            taskId: TaskId::fromString($command->getTaskId()),
+            user: User::fromString($command->getUser()),
+            title: Title::fromString($command->getTitle()),
+            description: Description::fromString($command->getDescription()),
+            status: Status::fromInt($command->getStatus()),
+            date: Date::create($command->getDate())
+        );
+        $this->taskService->update($task);
+    }
 }
