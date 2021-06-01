40e37b25b14ec1b469b17ec9dc0090548f02686e
Mamikon Arakelyan <m.araqelyan@gmail.com>
2021-06-02 02:26:49 +0400

    Implemented Task UUID can be created from string and it will throw exception if it is invalid

--- src/TaskManagement/Domain/Task/TaskId.php
+++ src/TaskManagement/Domain/Task/TaskId.php
@@ -5,6 +5,7 @@ namespace TaskManagement\Domain\Task;
 
 
 use Ramsey\Uuid\Uuid;
+use TaskManagement\Domain\Task\Exception\InvalidUuidException;
 
 class TaskId
 {
@@ -16,6 +17,15 @@ class TaskId
     public static function generate(): self
     {
         return new self(Uuid::uuid4()->toString());
+    }
+
+    public static function fromString(string $id): self
+    {
+        if (!Uuid::isValid($id)) {
+            throw new InvalidUuidException(sprintf("Task Id can't be created invalid Uuid provided- %s", $id));
+        }
+
+        return new self($id);
     }
 
     public function toString(): string
