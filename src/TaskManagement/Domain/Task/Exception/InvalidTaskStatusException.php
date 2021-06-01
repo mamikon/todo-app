1a06bf6e689d88a7594c74b2e38763ebeb9a66ab
Mamikon Arakelyan <m.araqelyan@gmail.com>
2021-06-01 04:29:49 +0400

    Not declared status throws exception

---
+++ src/TaskManagement/Domain/Task/Exception/InvalidTaskStatusException.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace TaskManagement\Domain\Task\Exception;
+
+
+use \InvalidArgumentException;
+
+class InvalidTaskStatusException extends InvalidArgumentException
+{
+
+}
--- src/TaskManagement/Domain/Task/Status.php
+++ src/TaskManagement/Domain/Task/Status.php
@@ -4,19 +4,41 @@
 namespace TaskManagement\Domain\Task;
 
 
+use TaskManagement\Domain\Task\Exception\InvalidTaskStatusException;
+
 class Status
 {
 
     const INCOMPLETE = 1;
+    const COMPLETED  = 2;
+    const DRAFT      = 3;
 
+    private static array $availableStatuses = [self::INCOMPLETE, self::COMPLETED, self::DRAFT];
 
     public function __construct(private int $status)
     {
 
     }
 
-    public static function fromInt(int $status)
+    /**
+     * @throws InvalidTaskStatusException
+     */
+    public static function fromInt(int $status): self
     {
+        if (!in_array($status, self::$availableStatuses)) {
+            throw new InvalidTaskStatusException(
+                sprintf(
+                    "Provided status(%d) is invalid. Available statuses are %s",
+                    $status,
+                    implode(",", self::$availableStatuses))
+            );
+        }
+
         return new self($status);
+    }
+
+    public static function getAvailableStatuses(): array
+    {
+        return self::$availableStatuses;
     }
 }
