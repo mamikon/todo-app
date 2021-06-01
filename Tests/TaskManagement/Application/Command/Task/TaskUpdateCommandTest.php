e8a326c4687343e6390321a761e26b115a383c8d
Mamikon Arakelyan <m.araqelyan@gmail.com>
2021-06-02 02:20:29 +0400

    Test that update handler working correctly

--- Tests/TaskManagement/Application/Command/Task/TaskUpdateCommandTest.php
+++ Tests/TaskManagement/Application/Command/Task/TaskUpdateCommandTest.php
@@ -1,6 +1,14 @@
 <?php
 
 
+use TaskManagement\Domain\Task\Date;
+use TaskManagement\Domain\Task\Description;
+use TaskManagement\Domain\Task\Status;
+use TaskManagement\Domain\Task\Task;
+use TaskManagement\Domain\Task\TaskId;
+use TaskManagement\Domain\Task\Title;
+use TaskManagement\Domain\Task\User;
+
 class TaskUpdateCommandTest extends \PHPUnit\Framework\TestCase
 {
     public function test_update_command_can_return_fields()
@@ -23,5 +31,39 @@ class TaskUpdateCommandTest extends \PHPUnit\Framework\TestCase
         $this->assertSame($command->getTitle(), 'title');
         $this->assertSame($command->getDescription(), 'description');
         $this->assertSame($command->getUser(), $user->toString());
+    }
+
+    public function test_task_update_handler_can_update_task()
+    {
+        require_once(__DIR__ . '/../../../Domain/Task/stubs/InMemoryRepository.php');
+        $repository  = new \TaskManagement\Domain\Task\stubs\InMemoryRepository();
+        $taskService = new \TaskManagement\Domain\Task\TaskService($repository);
+        $handler     = new \TaskManagement\Application\Command\Task\TaskUpdateHandler($taskService);
+        $taskId      = TaskId::generate();
+        $task        = Task::create(
+            taskId: $taskId,
+            user: User::fromString(\Ramsey\Uuid\Uuid::uuid4()),
+            title: Title::fromString("title"),
+            description: Description::fromString("description"),
+            status: Status::fromInt(Status::INCOMPLETE),
+            date: Date::create(new DateTimeImmutable())
+        );
+        $taskService->store($task);
+        $command = new \TaskManagement\Application\Command\Task\TaskUpdateCommand(
+            taskId: $taskId->toString(),
+            user: \Ramsey\Uuid\Uuid::uuid4(),
+            title: "title updated",
+            date: new DateTimeImmutable('2000-02-10 16:00:00'),
+            description: "description updated",
+            status: \TaskManagement\Domain\Task\Status::COMPLETED
+        );
+        $handler($command);
+        $updatedTask = $taskService->getById($taskId);
+        $this->assertSame($updatedTask->getTaskId()->toString(), $task->getTaskId()->toString());
+        $this->assertNotSame($updatedTask->getDate()->toString(), $task->getDate()->toString());
+        $this->assertNotSame($updatedTask->getTitle()->toString(), $task->getTitle()->toString());
+        $this->assertNotSame($updatedTask->getDescription()->toString(), $task->getDescription()->toString());
+        $this->assertNotSame($updatedTask->getUser()->toString(), $task->getUser()->toString());
+        $this->assertNotSame($updatedTask->getStatus()->getValue(), $task->getStatus()->getValue());
     }
 }
